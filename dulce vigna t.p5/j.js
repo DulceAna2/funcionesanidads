dbb174847b94bc04331897183e947fcc125f9a82
DulceAna2 <142264165+DulceAna2@users.noreply.github.com>
2024-05-13 17:05:58 -0300

    Update j.js

--- dulce vigna t.p5/j.js
+++ dulce vigna t.p5/j.js
@@ -1,44 +1,46 @@
-function conventirUnidad(cantidad, unidadOriginal) {
+//FUNCION GRANDE
+function conventirUnidad(){
+    let cantidad=parseInt(document.getElementById("cantidad").value);
+    let unidadOriginal=document.getElementById("medida_original").value;
+    console.log("el valor del select es:"+unidadOriginal)
+    console.log("el valor del input es:"+cantidad)
+    //SUB FUNCIONES
     function metrosA_pies(metros) {
-        return metros * 3.281;
+        return (metros * 3.281).toFixed(2);
     }
     function kilogramosA_libras(kilogramos) {
-        return kilogramos * 2.20462
+        return (kilogramos * 2.20462).toFixed(2);
     }
     function kilometrosA_millas(kilometros) {
-        return kilometros / 1.609
+        return (kilometros / 1.609).toFixed(2);
     }
     function litrosA_galones(litros) {
-        return litros / 3.785
+        return (litros / 3.785).toFixed(2);
     }
     function centrimetrosA_pulgadas(centimetros) {
-        return centimetros / 2.54
+        return (centimetros / 2.54).toFixed(2);
     }
     let resultado;
+    //CLASIFICAR LAS unidadOriginal
     switch (unidadOriginal.toLowerCase()) {
         case "metros":
             resultado = metrosA_pies(cantidad);
-            return `La cantidad ${cantidad} metros es equivalentes a ${resultado.toFixed(2)} pies`;
+            return alert("La cantidad "+cantidad+"metros es equivalentes a" + resultado+" pies")
+            //no se necesita break porque el return(retorna) las opciones
         case "kilogramos":
             resultado = kilogramosA_libras(cantidad);
-            return `La cantidad ${cantidad} kilogramos es equivalentes a ${resultado.toFixed(2)} libras`;
+            return  alert("La cantidad "+ cantidad+" kilogramos es equivalentes a "+ resultado+" libras")
         case "kilometros":
             resultado = kilometrosA_millas(cantidad);
-            return `La cantidad ${cantidad} kilometros es equivalentes a ${resultado.toFixed(2)} millas`;
+            return  alert("La cantidad "+ cantidad+" kilometros es equivalentes a "+ resultado+" millas")
         case "litros":
             resultado = litrosA_galones(cantidad);
-            return `La cantidad ${cantidad} litros es equivalentes a ${resultado.toFixed(2)} galones`;
-        case "centrimetros":
+            return  alert("La cantidad "+ cantidad+" litros es equivalentes a "+ resultado+" galones")
+        case " centimetros":
             resultado = centrimetrosA_pulgadas(cantidad);
-            return `La cantidad ${cantidad} centimetros es equivalentes a ${resultado.toFixed(2)} pulgadas`;
+            return alert("La cantidad "+ cantidad+" centimetros es equivalentes a "+ resultado+" pulgadas")
         default:
-            return `Unidad de medida no compatible`;
-
+            return alert(`Unidad de medida no compatible`);
     }
-
+    
 }
-console.log(conventirUnidad(5, "metros"))
-console.log(conventirUnidad(5, "kilogramos"))
-console.log(conventirUnidad(5, "kilometros"))
-console.log(conventirUnidad(5, "litros"))
-console.log(conventirUnidad(5, "centrimetros"))
